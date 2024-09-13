737d8e631aa86c5317309c99ea396c9f026f1505
Alevv1 <alejandrovallejos123@gmail.com>
2024-09-13 12:52:32 -0300

    database

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
     <!-- Bootstrap CSS -->
-    @vite(['resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <!-- Livewire Styles -->
     @livewireStyles
