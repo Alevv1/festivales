94b93a2c9e3085a0876a6f5fdf52ffac98d05558
Alevv1 <alejandrovallejos123@gmail.com>
2024-09-13 13:34:10 -0300

    database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            DJSeeder::class,
             FestivalSeeder::class,
             RoleAndPermissionSeeder::class,
 
